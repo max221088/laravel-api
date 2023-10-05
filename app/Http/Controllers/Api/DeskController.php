e541e501f5c45941d13e5082bea543bb4b60d861
max.work221088 <max.work221088@gmail.com>
2023-10-05 14:28:55 +0300

    added desks get

---
+++ app/Http/Controllers/Api/DeskController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Desk;
+use App\Http\Resources\DeskResource;
+
+class DeskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return DeskResource::collection(Desk::with('lists')->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+			return new DeskResource(Desk::with('lists')->findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
